68ff650063d7857c3a52b7f38b59a6e5cd2db28c
Luis Guerra <guerraherrera000@gmail.com>
2026-09-08 02:24:19 -0500

    Configurar pruebas y SonarQube para backend y frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,15 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js',
+    coverage: {
+
+      provider: 'v8',
+      reporter: ['lcov', 'text'],
+      exclude: ['node_modules/', 'eslint.config.js', 'postcss.config.js', 'tailwind.config.js', 'vite.config.js'],
+    },
+  },
 });
